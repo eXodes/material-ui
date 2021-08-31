15acbd6273f83d5b5103a56901a26103f728d347
Exo Des <hello@exodes.net>
2021-08-31 17:45:23 +0800

    build: inject `react` by default

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    jsxInject: `import React from 'react'`
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
