c26758179e542f0fcf2d2cffa8ead389415137cd
Exo Des <hello@exodes.net>
2021-08-31 19:04:10 +0800

    feat: update markup to `material-ui`

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,84 @@
 import { useState } from 'react'
 import logo from '@/assets/logo.svg'
 import '@/App.css'
+import { Paper, Button, Grid, Container, Link, Typography, Box, useMediaQuery, styled } from "@material-ui/core"
+
+const Image = styled('img')(() =>  ({
+  height: '40vmin',
+  pointerEvents: 'none',
+}))
 
 function App() {
   const [count, setCount] = useState(0)
+  const allowMotion = useMediaQuery('(prefers-reduced-motion: no-preference)');
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Grid container sx={{
+      backgroundColor: '#f6f9ff',
+    }}>
+      <Container sx={{
+        maxWidth: 'lg',
+        minHeight: '100vh',
+      }}>
+        <Paper elevation={2} sx={{
+          mt: 4,
+          textAlign: 'center',
+          padding: '1rem 2rem',
+        }}>
+
+          <Image src={logo} alt="logo" sx={{
+            animation: allowMotion ? 'spin infinite 20s linear' : ''
+          }} 
+          />
+
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '2rem',
+          }}>
+
+          <Typography variant="h3">Hello Vite + React + Material UI!</Typography>
+
+          <Box>
+            <Button variant="contained" disableElevation type="button" onClick={() => setCount((count) => count + 1)}>
+              count is: {count}
+            </Button>
+          </Box>
+
+          <Typography variant="body1">
+            Edit <code>App.tsx</code> and save to test HMR updates.
+          </Typography>
+
+          <Typography variant="body2">
+          <Link
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </Link>
+            {' | '}
+            <Link
+              href="https://next.material-ui.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn Material UI
+            </Link>
+            {' | '}
+            <Link
+              href="https://vitejs.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Vite Docs
+            </Link>
+          </Typography>
+
+          </Box>
+        </Paper>
+      </Container>
+    </Grid>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { render } from 'react-dom'
 import '@/index.css'
 import App from '@/App'
+import { CssBaseline } from "@material-ui/core"
 
 render(
   <StrictMode>
-    <App />
+    <CssBaseline>
+      <App />      
+    </CssBaseline>
   </StrictMode>,
   document.getElementById('root')
 )
