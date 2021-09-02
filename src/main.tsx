83035a1308dd7e791a65000e15fe1c77ed9d1f68
Exo Des <hello@exodes.net>
2021-09-02 23:59:48 +0800

    build: add `eslint` & `prettier`

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,14 @@ import '@/index.css'
 import App from '@/App'
 import { CssBaseline } from "@material-ui/core"
 
+if (process.env.NODE_ENV !== "production") {
+  import("@axe-core/react").then((axe) => axe.default(import("react"), import("react-dom"), 1000));
+}
+
 render(
   <StrictMode>
     <CssBaseline>
-      <App />      
+      <App />
     </CssBaseline>
   </StrictMode>,
   document.getElementById('root')
