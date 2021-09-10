1547659f813d11b697844ee66d065d1932690a0c
Exo Des <hello@exodes.net>
2021-09-11 04:20:34 +0800

    docs: add @jsdocs comment

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,9 @@
 import { PRODUCTION } from "@/constants";
 
+/**
+ * Check if the current environment is production
+ * @param string Enviroment to check
+ * @returns boolean true if enviroment is production
+ */
 export const isProduction = (string: string) =>
     string.toLowerCase() === PRODUCTION;
