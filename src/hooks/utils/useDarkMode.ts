a599fab119c0be68d1a88f0f7fc91a8da0310d42
Exo Des <hello@exodes.net>
2021-09-13 22:55:24 +0800

    feat: add `useDarkMode` hook

---
+++ src/hooks/utils/useDarkMode.ts
@@ -0,0 +1,39 @@
+import { useEffect, useMemo, useState } from "react";
+import { createTheme, PaletteMode, useMediaQuery } from "@material-ui/core";
+import { useLocalStorage } from "react-use";
+import { getPaletteTokens } from "@/helpers/theme";
+import { THEME_KEY, THEME_MODE } from "@/constants/theme";
+
+export const useDarkMode = () => {
+    const [localMode, setLocalMode] = useLocalStorage<THEME_MODE>(THEME_KEY);
+    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+    const [mode, setMode] = useState<PaletteMode>(
+        prefersDarkMode ? THEME_MODE.DARK : THEME_MODE.LIGHT,
+    );
+
+    const theme = useMemo(() => createTheme(getPaletteTokens(mode)), [mode]);
+
+    const toggleMode = (value: THEME_MODE | null = null) => {
+        setMode((prev) => {
+            const current =
+                value ?? prev === THEME_MODE.DARK
+                    ? THEME_MODE.LIGHT
+                    : THEME_MODE.DARK;
+
+            setLocalMode(current);
+
+            return current;
+        });
+    };
+
+    useEffect(() => {
+        localMode
+            ? setMode(localMode)
+            : prefersDarkMode
+            ? setMode(THEME_MODE.DARK)
+            : setMode(THEME_MODE.LIGHT);
+    }, [localMode, prefersDarkMode]);
+
+    return { theme, mode, toggleMode };
+};
