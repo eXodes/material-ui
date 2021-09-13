0f3caa52ba790ed9e0b1cfa6f90fcd77c2c6e955
Exo Des <hello@exodes.net>
2021-09-14 00:13:52 +0800

    feat: add theme toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import logo from "@/assets/logo.svg";
 import "@/App.css";
 import {
@@ -6,112 +6,153 @@ import {
     Button,
     Container,
     Grid,
+    IconButton,
     Link,
     Paper,
     styled,
+    ThemeProvider,
     Typography,
     useMediaQuery,
 } from "@material-ui/core";
+import { MoonIcon, SunIcon } from "@heroicons/react/solid";
+import { ThemeContext } from "@/contexts/theme";
+import { THEME_MODE } from "@/constants/theme";
 
 const Image = styled("img")(() => ({
     height: "40vmin",
     pointerEvents: "none",
 }));
 
+const LightModeIcon = styled(SunIcon)({
+    width: "2rem",
+});
+
+const DarkModeIcon = styled(MoonIcon)({
+    width: "2rem",
+});
+
 const App = () => {
+    const { theme, mode, toggleMode } = useContext(ThemeContext);
     const [count, setCount] = useState(0);
     const allowMotion = useMediaQuery(
         "(prefers-reduced-motion: no-preference)",
     );
 
     return (
-        <Grid
-            container
-            sx={{
-                backgroundColor: "#f6f9ff",
-            }}
-        >
-            <Container
+        <ThemeProvider theme={theme}>
+            <Grid
+                container
                 sx={{
-                    maxWidth: "lg",
-                    minHeight: "100vh",
+                    backgroundColor: "background.paper",
                 }}
             >
-                <Paper
-                    elevation={2}
+                <Container
                     sx={{
-                        mt: 4,
-                        textAlign: "center",
-                        padding: "1rem 2rem",
+                        maxWidth: "lg",
+                        minHeight: "100vh",
                     }}
                 >
-                    <Image
-                        src={logo}
-                        alt="logo"
+                    <Paper
+                        elevation={2}
                         sx={{
-                            animation: allowMotion
-                                ? "spin infinite 20s linear"
-                                : "",
-                        }}
-                    />
-
-                    <Box
-                        sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            gap: "2rem",
+                            mt: 4,
+                            borderRadius: 5,
+                            textAlign: "center",
+                            padding: "1rem 2rem",
                         }}
                     >
-                        <Typography variant="h3">
-                            Hello Vite + React + Material UI!
-                        </Typography>
+                        <Image
+                            src={logo}
+                            alt="logo"
+                            sx={{
+                                animation: allowMotion
+                                    ? "spin infinite 20s linear"
+                                    : "",
+                            }}
+                        />
 
-                        <Box>
-                            <Button
-                                variant="outlined"
-                                onClick={() => setCount((count) => count + 1)}
+                        <Box
+                            sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                gap: "2rem",
+                            }}
+                        >
+                            <Typography variant="h3">
+                                Hello Vite + React + Material UI!
+                            </Typography>
+
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    gap: "1rem",
+                                }}
                             >
-                                <span>
-                                    count is:
-                                    <span data-testid="count"> {count}</span>
-                                </span>
-                            </Button>
+                                <Button
+                                    size="large"
+                                    variant="outlined"
+                                    onClick={() =>
+                                        setCount((count) => count + 1)
+                                    }
+                                >
+                                    <span>
+                                        count is:
+                                        <span data-testid="count">
+                                            {" "}
+                                            {count}
+                                        </span>
+                                    </span>
+                                </Button>
+
+                                <IconButton
+                                    onClick={() => toggleMode()}
+                                    color="warning"
+                                    aria-label="Toggle dark mode"
+                                >
+                                    {mode === THEME_MODE.LIGHT ? (
+                                        <LightModeIcon />
+                                    ) : (
+                                        <DarkModeIcon />
+                                    )}
+                                </IconButton>
+                            </Box>
+
+                            <Typography variant="body1">
+                                Edit <code>App.tsx</code> and save to test HMR
+                                updates.
+                            </Typography>
+
+                            <Typography variant="body2">
+                                <Link
+                                    href="https://reactjs.org"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Learn React
+                                </Link>
+                                {" | "}
+                                <Link
+                                    href="https://next.material-ui.com"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Learn Material UI
+                                </Link>
+                                {" | "}
+                                <Link
+                                    href="https://vitejs.dev/guide/features.html"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Vite Docs
+                                </Link>
+                            </Typography>
                         </Box>
-
-                        <Typography variant="body1">
-                            Edit <code>App.tsx</code> and save to test HMR
-                            updates.
-                        </Typography>
-
-                        <Typography variant="body2">
-                            <Link
-                                href="https://reactjs.org"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                Learn React
-                            </Link>
-                            {" | "}
-                            <Link
-                                href="https://next.material-ui.com"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                Learn Material UI
-                            </Link>
-                            {" | "}
-                            <Link
-                                href="https://vitejs.dev/guide/features.html"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                Vite Docs
-                            </Link>
-                        </Typography>
-                    </Box>
-                </Paper>
-            </Container>
-        </Grid>
+                    </Paper>
+                </Container>
+            </Grid>
+        </ThemeProvider>
     );
 };
 
