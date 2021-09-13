6436b187ca297b0a8b6397e167a43ccaa840d045
Exo Des <hello@exodes.net>
2021-09-13 22:54:59 +0800

    refactor: rename `getDesignTokens` to `getPaletteTokens`

--- src/helpers/theme.ts
+++ src/helpers/theme.ts
@@ -1,8 +1,8 @@
 import { PaletteMode } from "@material-ui/core";
 import { darkOptions, lightOptions } from "@/configs/theme";
-import { THEME_MODE } from "@/constants";
+import { THEME_MODE } from "@/constants/theme";
 
-export const getDesignTokens = (mode: PaletteMode) => ({
+export const getPaletteTokens = (mode: PaletteMode) => ({
     palette: {
         mode,
         ...(mode === THEME_MODE.LIGHT ? lightOptions : darkOptions),
